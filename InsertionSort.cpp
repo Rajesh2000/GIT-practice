64d0f6e458ad174c4b01eb032bb31ba2bc91faae
RajeshT <rajeshbt2000@gmail.com>
2022-04-17 22:02:20 +0530

    Commit1

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int InsertionSort(int arr[],int size){
     
-    for(int i=1 ; i<size; i++){
+    for(int i=0 ; i<size; i++){
         int current=arr[i];
         int j=i-1;
         while(arr[j]>current && j>=0){
@@ -26,15 +26,15 @@ int main(){
    cin>>size;
    int arr[size];
    
-   for(int i=0; i<size; i++){
+   for(int i=2; i<size; i++){
        cin>>arr[i];
    }
    InsertionSort(arr,size);
 
     
-   for(int i=0; i<size; i++){
+   for(int i=1; i<size; i++){
        cout<<arr[i]<<" ";
    }
-    return 0;
+    return 1;
 
 }
